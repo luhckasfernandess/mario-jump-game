c62400223015c2fca295d5c593092c47bc32bdf5
Lucas Fernandes <luhckaspessoal@gmail.com>
2023-02-23 21:51:20 -0400

    feat: para a animação das nuvens no game over :end:

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const hero = document.querySelector('.game-hero');
+const gameHero = document.querySelector('.game-hero');
 const gameObstacle = document.querySelector('.game-obstacle');
+const gameClouds = document.querySelector('.game-clouds');
 
 const heroSkill = () => {
-    hero.classList.add('hero-skill');
+    gameHero.classList.add('hero-skill');
     setTimeout(() => hero.classList.remove('hero-skill'), 500);
 }
 
 const verificationLoop = setInterval(() => {
 
     const obstaclePosition = gameObstacle.offsetLeft;
-    const heroPosition = +window.getComputedStyle(hero).bottom.replace('px', '');
+    const cloudsPosition = gameClouds.offsetLeft;
+    const heroPosition = +window.getComputedStyle(gameHero).bottom.replace('px', '');
 
     if (obstaclePosition <= 120 && obstaclePosition > 0 && heroPosition <= 125) {
 
         gameObstacle.style.animation = 'none';
         gameObstacle.style.left = `${obstaclePosition}px`;
 
-        hero.style.animation = 'none';
-        hero.style.bottom = `${heroPosition}px`;
-        hero.src = './images/game-over.webp'
-        hero.style.width = '100px'
-        hero.style.marginLeft = '35px'
+        gameHero.style.animation = 'none';
+        gameHero.style.bottom = `${heroPosition}px`;
+        gameHero.src = './images/game-over.webp'
+        gameHero.style.width = '100px'
+        gameHero.style.marginLeft = '35px'
+
+        gameClouds.style.animation = 'none';
+        gameClouds.style.left = `${cloudsPosition}px`;
 
         clearInterval(verificationLoop);
     }
