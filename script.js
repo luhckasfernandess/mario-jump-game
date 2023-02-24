1dc62219b0aa214fe3dbb0eb8c850e04e74d2e88
Lucas Fernandes <luhckaspessoal@gmail.com>
2023-02-23 20:25:26 -0400

    feat: adiciona regras para game over :video_game:

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+const hero = document.querySelector('.game-hero');
+const gameObstacle = document.querySelector('.game-obstacle');
+
 const heroSkill = () => {
-    const hero = document.querySelector('.game-hero');
     hero.classList.add('hero-skill');
     setTimeout(() => hero.classList.remove('hero-skill'), 500);
 }
+
+const verificationLoop = setInterval(() => {
+
+    const obstaclePosition = gameObstacle.offsetLeft;
+    const heroPosition = +window.getComputedStyle(hero).bottom.replace('px', '');
+
+    if (obstaclePosition <= 120 && obstaclePosition > 0 && heroPosition <= 80) {
+
+        gameObstacle.style.animation = 'none';
+        gameObstacle.style.left = `${obstaclePosition}px`;
+
+        hero.style.animation = 'none';
+        hero.style.bottom = `${heroPosition}px`;
+    }
+}, 10);
 
 document.addEventListener('keydown', heroSkill);
